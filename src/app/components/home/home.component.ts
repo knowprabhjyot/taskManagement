76b03258abde181291e72cb1b1e3ab4cd0beb782
Prabhjyot <knowprabhjyot@gmail.com>
2021-01-15 00:10:28 +0530

    removed sidenav since less action buttons

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Task } from 'src/app/models/task';
 import { TaskService } from 'src/app/services/task.service';
+import { AddTaskComponent } from '../add-task/add-task.component';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +11,7 @@ import { TaskService } from 'src/app/services/task.service';
 })
 export class HomeComponent implements OnInit {
   public taskList: Task[] = [];
-  constructor(private taskService: TaskService) { }
+  constructor(private taskService: TaskService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.getTaskList();
@@ -20,5 +22,17 @@ export class HomeComponent implements OnInit {
       this.taskList = response;
     })
   }
+
+  
+  public createTask(): void {
+    const dialogRef = this.dialog.open(AddTaskComponent, {
+      width: '400px'
+    });
+    
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
 
 }
