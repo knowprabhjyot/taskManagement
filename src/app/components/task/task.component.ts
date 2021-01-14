dd1ab57fc754d3664705e2ae0b9cd2d946f8d8f5
Prabhjyot <knowprabhjyot@gmail.com>
2021-01-15 00:36:40 +0530

    delete task feature added

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TaskService } from 'src/app/services/task.service';
 import { Task } from '../../models/task';
+import { AddTaskComponent } from '../add-task/add-task.component';
 
 @Component({
   selector: 'app-task',
@@ -8,5 +12,24 @@ import { Task } from '../../models/task';
 })
 export class TaskComponent {
   @Input() public task: Task;
-  constructor() { }
+  constructor(private dialog: MatDialog, private taskService: TaskService, private snackBar: MatSnackBar) { }
+
+  public editTask(): void {
+    const dialogRef = this.dialog.open(AddTaskComponent, {
+      width: '400px',
+      data: this.task
+    });
+    
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  public deleteTask(): void {
+    console.log(this.task.id);
+    const response = this.taskService.deleteTask(this.task.id);
+    if (response.status === 200) {
+      this.snackBar.open(response.message, null, { duration: 2000 });
+    }
+  }
 }
