7fbd2c79e2976b82afe67538739741ced309e63b
Prabhjyot <knowprabhjyot@gmail.com>
2021-01-15 21:25:32 +0530

    code modified

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,7 +12,8 @@ import { AddUserComponent } from '../add-user/add-user.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  
 })
 export class HomeComponent implements OnInit, OnDestroy {
   public taskList: Task[] = [];
